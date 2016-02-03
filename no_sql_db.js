ddd1e6f0e46e7730e7f8f359298365a3f6377714
Mike Hannon <mjhannon@gmail.com>
2016-02-03 07:09:55 -0800

    error filled day2

--- no_sql_db.js
+++ no_sql_db.js
@@ -18,6 +18,8 @@ function Echidna(name){
   // multiple tables will go into this.
   // option 1: this is fine
   this.tables = {};
+    // NEW AS OF DAY2
+  this.results;
 
   // our table constructor
   this.createTable = function(name, schema){
@@ -29,10 +31,18 @@ function Echidna(name){
     this.tables[name] = new EchidnaTable(name,schema)
     // this.tables.push(new EchidnaTable(name,schema));
   }
-  this.create = function(table_name, data){
-    this.tables[table_name].data.push(data);
+  this.createTableData = function(name, data){
+    console.log(this.tables[name], "friends?");
+    this.tables[name].create(data);
     return this;
   }
+  // NEW AS OF DAY2
+  this.findTableData = function(name, key, value){
+    this.results = this.tables[name].findData(key,value);
+    return this;
+  }
+
+
   // write a new function that adds data to our table by name!
 
 }
@@ -42,21 +52,47 @@ function EchidnaTable(name, schema){
   this.schema = schema;
   // maybe this should be an object?
   this.data = [];
+  // left data as an array... maybe make it a (multi?) heap though--future impl
+  //moved create in the table, since this method is for the table!
+  this.create = function(data){
+    console.log("in create")
+    var hash = "";
+    // make an id for each which will only rarely be duplicated (a hash key).
+    for (var i = 0; i < 14; i ++){
+      hash += Math.floor(Math.random()*10);
+    }
+    // uses that hash
+    data._id = hash;
+    this.data.push(data);
+    console.log(this);
+    return this;
+  }
+
+  // there are a few things wrong here! get it to work!
+    // NEW AS OF DAY2
+  this.findData = function(skey, svalue){
+    var return_array =[ ];
+    data.forEach(function(tvalue, tkey){
+      // console.log(tvalue, "tvalue", skey);
+      // console.log(tvalue[skey]);
+      if (tvalue[skey] == svalue){
+        return_array.push(tvalue);
+      }
+    });
+
+  }
 }
 
-//1 Fix the code!
-//2 Make the test below succeed (uncomment it and make it work!)
-//TEST CODE
+
 var myDB = new Echidna("First_DB");
-console.log(myDB);
+
 myDB.createTable('friends');
-console.log(myDB);
+
 // MAKE THIS WORK!
-myDB.create('friends',{name:"Charlie",occupation:"Bootcamp Leader"});
-console.log(myDB);
-console.log(myDB.tables['friends'].data);
-console.log(myDB.tables.friends.data);
+myDB.createTableData('friends',{name:"Charlie",occupation:"Bootcamp Leader"});
+// console.log(myDB);
+// console.log(myDB.tables['friends'].data);
 
-
-
+myDB.findTableData('friends', "name", "Charlie");
+// expect a return of [{_id: 12312531515, name:"Charlie",occupation:"Bootcamp Leader"}] or something similar - get it to work!
 // where myDB now has a table of friends with a record of {name:"Charlie",occupation:"Bootcamp Leader"}.
